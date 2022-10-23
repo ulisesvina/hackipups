1b1789f92b3aad960a9f230785f799849692cfc3
ProfMonkey07 <caldermcqueen@gmail.com>
2022-10-22 21:12:43 -0700

    more shop stuff

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,7 @@
-var items = [{name: "pumpkin hat", price: 100, img: 'https://cdn.discordapp.com/attachments/1033175931688333354/1033583634558492753/unknown.png'}, {name: "skull hat", price:200, img: 'https://cdn.discordapp.com/attachments/1033175931688333354/1033583836816228412/unknown.png'},
-{name: 'beret', price: 150, img: 'https://cdn.discordapp.com/attachments/1033175931688333354/1033583984132759612/unknown.png'},
-{name: 'top hat', price: 300, img: 'https://cdn.discordapp.com/attachments/1033175931688333354/1033584222000136222/unknown.png'},
-{name: 'bow', price: 100, img: 'https://cdn.discordapp.com/attachments/1033175931688333354/1033584378003079220/unknown.png'}];
+var items = [{key: 0, owned: false, name: "pumpkin hat", price: 100, img: 'https://cdn.discordapp.com/attachments/1033175931688333354/1033583634558492753/unknown.png'}, {key: 1, owned: false, name: "skull hat", price:200, img: 'https://cdn.discordapp.com/attachments/1033175931688333354/1033583836816228412/unknown.png'},
+{key: 2, owned: false, name: 'beret', price: 150, img: 'https://cdn.discordapp.com/attachments/1033175931688333354/1033583984132759612/unknown.png'},
+{key: 3, owned: false, name: 'top hat', price: 300, img: 'https://cdn.discordapp.com/attachments/1033175931688333354/1033584222000136222/unknown.png'},
+{key: 4, owned: false, name: 'bow', price: 100, img: 'https://cdn.discordapp.com/attachments/1033175931688333354/1033584378003079220/unknown.png'}];
 import { atom, useRecoilValue } from 'recoil'
 const shopState = atom({
   key: 'shopState', // unique ID (with respect to other atoms/selectors)
@@ -16,10 +16,11 @@ const Shop = () => {
         <table style={{display: "flex", flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
           {shop.map(item =>{ 
             return(
-              <tr key={item.name} style={{background: "blue"}}>
+              <tr key={item.key} style={{background: "blue", margin: "10px", borderRadius: "10px"}}>
               <th>
                 {item.name}
                 </th>
+                <br/>
               <th style={{color: "gold"}}>{item.price}</th>
               <th>
                 <img src={item.img} />
