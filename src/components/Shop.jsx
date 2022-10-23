899cb6002f23176689f21c5e797b92ed73c4276f
April Larsen <hunterapril94@gmail.com>
2022-10-22 19:24:53 -0700

    recoil for shop and stats

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,16 @@
 var items = [{name: "tophat", price: 100}, {name: "beret", price:500}];
-
+import { atom, useRecoilValue } from 'recoil'
+const shopState = atom({
+  key: 'shopState', // unique ID (with respect to other atoms/selectors)
+  default: items, // default value (aka initial value)
+});
 const Shop = () => {
+  const shop = useRecoilValue(shopState)
     return(
     <div>shop
         <br/>
 
-        <ul style={{display: "flex", flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>{items.map(item => {
+        <ul style={{display: "flex", flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>{shop.map(item => {
             return(<li key={item.name} style={{background: "blue"}}>
                 {item.name}
                 <br/>
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,13 +1,19 @@
 import { useEffect } from "react";
-var stats = {hunger: 100, boredom: 100, energy: 100, thirst: 100};
+import { atom, useRecoilValue } from 'recoil'
+
+const statsState = atom({
+  key: 'statsState', // unique ID (with respect to other atoms/selectors)
+  default: {hunger: 100, boredom: 100, energy: 100, thirst: 100}, // default value (aka initial value)
+});
 const Stats = () => {
-  useEffect(() => {
-    if(localStorage.getItem('stats') == null){
-        localStorage.setItem('stats', JSON.stringify(stats));
-    } else {
-        stats = JSON.parse(localStorage.getItem('stats'));
-    }
-  }, []);
+  const stats = useRecoilValue(statsState)
+  // useEffect(() => {
+  //   if(localStorage.getItem('stats') == null){
+  //       localStorage.setItem('stats', JSON.stringify(stats));
+  //   } else {
+  //       stats = JSON.parse(localStorage.getItem('stats'));
+  //   }
+  // }, []);
     return(
         <div>
             <b>hunger: {stats.hunger}</b>
